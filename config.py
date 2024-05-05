3f3bb0a531692fe1739aafe6e2fc27ad3657dc2f
DeveloperTharun <114932079+DeveloperTharun@users.noreply.github.com>
2024-05-05 08:13:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://SkMedia:Tharunraj1828@cluster0.vbdxs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "fileshareexbot")
+DB_NAME = os.environ.get("DATABASE_NAME", "tamilskmoviez2")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = lambda: int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
