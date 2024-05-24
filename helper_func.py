32fd3f8a92802427cf95ebd94bd63fb5b4e1e102
DeveloperTharun <114932079+DeveloperTharun@users.noreply.github.com>
2024-05-24 19:31:42 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -144,11 +144,11 @@ async def short_link(link):
     hour = current_time.hour
     
     if 1 <= hour < 13:  
-        api = "e36f2573636d8a5dd3b81b64ce434db80002867a"
-        site = "modijiurl.com"
+        api = "acda894b4b12a377de4341efe6232b6e6d0c5ea5"
+        site = "publicearn.com"
     else:  
-        api = "8b2aa79be0fe87e9299264924b6909117f2c0f22"
-        site = "modijiurl.com"
+        api = "acda894b4b12a377de4341efe6232b6e6d0c5ea5"
+        site = "publicearn.com"
     
     shortzy = Shortzy(api, site) 
     short_link = await shortzy.convert(link)
