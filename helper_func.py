630790dd3d726199b4e4e4be0a20fea21e710954
DeveloperTharun <114932079+DeveloperTharun@users.noreply.github.com>
2024-07-22 20:46:05 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -147,7 +147,7 @@ async def short_link(link):
         api = "e36f2573636d8a5dd3b81b64ce434db80002867a"
         site = "modijiurl.com"
     else:  
-        api = "8b2aa79be0fe87e9299264924b6909117f2c0f22"
+        api = "acda894b4b12a377de4341efe6232b6e6d0c5ea5"
         site = "modijiurl.com"
     
     shortzy = Shortzy(api, site) 
