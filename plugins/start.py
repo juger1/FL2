ed3b37eadf3c36dccc73fc36260acd3ee91c2803
Logesh <logesh.12a3@gmail.com>
2024-07-30 20:09:17 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -118,7 +118,7 @@ async def start_command(client: Client, message: Message):
             else:
                 caption = "" if not msg.caption else msg.caption.html
 
-            reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Main Channel", url="https://t.me/TamilSk_Moviez")]])
+            reply_markup = None
 
             try:
                 ss = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT())
