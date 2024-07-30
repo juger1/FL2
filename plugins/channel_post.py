8dc065dcdfb4c89ce3854fbf901ef010e73e283d
Logesh <logesh.12a3@gmail.com>
2024-07-30 19:48:25 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -63,18 +63,35 @@ async def channel_post(client: Client, message: Message):
         previouscaptions = None
     else:
         previouscaption = f"no caption"
-    file_name = previouscaptions
-    filesize = human_readable_size(get_media_file_size(message))
-    converted_id = post_message.id * abs(client.db_channel.id)
-    string = f"get-{converted_id}"
-    base64_string = await encode(string)
-    link = f"https://filestore.rapidbots.workers.dev?start={base64_string}"
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔓 OPEN URL", url=f'{link}')]])
-    await reply_text.edit(f"{filesize} - <a href='{link}'>Click Here To Download</a>", reply_markup=reply_markup, disable_web_page_preview = True)
+    if message.photo:
+        converted_id = post_message.id * abs(client.db_channel.id)
+        string = f"get-{converted_id}"
+        base64_string = await encode(string)
+        link = f"https://filestore.rapidbots.workers.dev?start={base64_string}"
+        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔓 OPEN URL", url=f'{link}')]])
+        await reply_text.edit(f"Link :\n`{link}`", reply_markup=reply_markup, disable_web_page_preview = True)
+        
+        if not DISABLE_CHANNEL_BUTTON:
+            await post_message.edit_reply_markup(reply_markup)
 
-    if not DISABLE_CHANNEL_BUTTON:
-        await post_message.edit_reply_markup(reply_markup)
+    else:
+        file_name = previouscaptions
+        filesize = human_readable_size(get_media_file_size(message))
+        converted_id = post_message.id * abs(client.db_channel.id)
+        string = f"get-{converted_id}"
+        base64_string = await encode(string)
+        link = f"https://filestore.rapidbots.workers.dev?start={base64_string}"
+
+        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔓 OPEN URL", url=f'{link}')]])
+        await reply_text.edit(f"{filesize} - <a href='{link}'>Click Here To Download</a>", reply_markup=reply_markup, disable_web_page_preview = True)
+        await asyncio.sleep(1)
+        await message.reply_text(f"<b>🌫 <a href='{link}'>{file_name}({filesize})</a></b>", reply_markup=reply_markup, disable_web_page_preview = True)
+        await asyncio.sleep(1)
+        await message.reply_text(f"<b>🌫 <a href='{link}'>{file_name}</a></b>", reply_markup=reply_markup, disable_web_page_preview = True)
+        
+        if not DISABLE_CHANNEL_BUTTON:
+            await post_message.edit_reply_markup(reply_markup)
 
 @Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID))
 async def new_post(client: Client, message: Message):
