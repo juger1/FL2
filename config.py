1fc66134d90a37ba18a4e3fb01a75b18a3b82aaf
DeveloperTharun <114932079+DeveloperTharun@users.noreply.github.com>
2024-08-13 13:33:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,8 @@ FURL = os.environ.get("FURL", "https://filestore.rapidbots.workers.dev?start=")
 #cloudflare permanent url eg. https://sk.tamilsk.workers.dev/
 CURL = os.environ.get("CURL", "https://sk.tamilsk.workers.dev/")
 
-STREAM = os.environ.get("STREAM", "false")
+#fast dl link if need set true else set false
+STREAM = os.environ.get("STREAM", "true")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://SkMedia:Tharunraj1828@cluster0.vbdxs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
