f225e9e4769aca482e8d9a28eb136e379d321d56
DeveloperTharun <114932079+DeveloperTharun@users.noreply.github.com>
2024-08-13 11:55:20 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -3,8 +3,10 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
-from config import ADMINS
+from config import ADMINS, FURL
 from helper_func import encode, get_message_id, get_media_file_size
+
+surl = FURL
 
 def human_readable_size(size):
     if size < 1024:
@@ -48,7 +50,7 @@ async def batch(client: Client, message: Message):
 
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
-    link = f"https://filestore.rapidbots.workers.dev?start={base64_string}"
+    link = f"{surl}{base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     await second_message.reply_text(f"<b>Here is your link</b>\n\n<code>{link}</code>", quote=True, reply_markup=reply_markup)
 
@@ -71,7 +73,7 @@ async def link_generator(client: Client, message: Message):
     filesize = human_readable_size(get_media_file_size(msg_id))
 
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
-    link = f"https://filestore.rapidbots.workers.dev?start={base64_string}"
+    link = f"{surl}{base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     
     reply_message = f"<b>Here is your link</b>\n\n<code>{link}</code>"
