9889188790cb2aff9b03f6de68348e415c0d8163
DeveloperTharun <114932079+DeveloperTharun@users.noreply.github.com>
2024-08-13 22:24:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ CURL = os.environ.get("CURL", "https://sk.tamilsk.workers.dev/")
 STREAM = os.environ.get("STREAM", "True")
 
 #Auto delete feature if need set True else set false (must use capital T)
-DELETE = os.environ.get("DELETE", "True")
+DELETE = os.environ.get("DELETE", "false")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://SkMedia:Tharunraj1828@cluster0.vbdxs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
