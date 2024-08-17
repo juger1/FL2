335326f087b249e62172bb0e8d153e2fcc9be7b1
Logesh <logesh.12a3@gmail.com>
2024-08-17 21:11:43 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -102,6 +102,14 @@ async def start_command(client: Client, message: Message):
                         )
 
     else:
+        reply_markup = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
+                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                ]
+            ]
+        )
         await message.reply_text(
             text = START_MSG.format(
                 first = message.from_user.first_name,
