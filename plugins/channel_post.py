412563f0b2f23ecbb6654396e276e5bfbca63eae
DeveloperTharun <114932079+DeveloperTharun@users.noreply.github.com>
2024-08-19 21:12:39 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -4,9 +4,9 @@ import asyncio
 from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
-
+from script import FURL
 from bot import Bot
-from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, FURL
+from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import encode, get_media_file_size
 
 surl = FURL
