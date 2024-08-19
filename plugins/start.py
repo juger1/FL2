ce9747bdfe8df4d51b351b779a32d2c91b85da1e
DeveloperTharun <114932079+DeveloperTharun@users.noreply.github.com>
2024-08-19 21:10:39 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -13,7 +13,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
 from bot import Bot
 from config import ADMINS, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, FL_CHANNEL, STREAM, DELETE, SRT_VERIFY, VERIFY
-from Script import CURL, VERIFY_MSG, VERIFIED_MSG, EVERIFY_MSG, FORCE_MSG, START_MSG, CUSTOM_CAPTION
+from script import CURL, VERIFY_MSG, VERIFIED_MSG, EVERIFY_MSG, FORCE_MSG, START_MSG, CUSTOM_CAPTION
 from helper_func import subscribed, encode, decode, get_messages, check_token, get_token, verify_user, check_verification
 from database.database import db
 import logging
