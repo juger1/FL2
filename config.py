a8b2867369583203ddf1d7ad6665afcf41b94d99
DeveloperTharun <114932079+DeveloperTharun@users.noreply.github.com>
2024-08-19 21:15:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,12 +22,6 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5669934860"))
 #Port
 PORT = os.environ.get("PORT", "8080")
 
-#permanent cloudflare file store url eg. https://filestore.rapidbots.workers.dev?start
-FURL = os.environ.get("FURL", "https://filestore.rapidbots.workers.dev?start=")
-
-#stream cloudflare permanent url eg. https://sk.tamilsk.workers.dev/
-CURL = os.environ.get("CURL", "https://sk.tamilsk.workers.dev/")
-
 #fast dl link if need set True else set false (must use capital T)
 STREAM = os.environ.get("STREAM", "True")
 
@@ -40,9 +34,6 @@ SRT_VERIFY = os.environ.get("SRT_VERIFY", "false")
 #start msgj without verify if need set True else set false (must use capital T)
 VERIFY = os.environ.get("VERIFY", "false")
 
-#else verified message if verify false it work 
-EVERIFY_MSG = os.environ.get("EVERIFY_MSG", "<b>Hello 👋🏻, You Need To Verify To Get Files, Open Below Bot For Verification. \n\nOpen @Sk_Verify_Bot & Start The Bot 👍</b>")
-
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://SkMedia:Tharunraj1828@cluster0.vbdxs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "tamilskmoviez2")
@@ -51,31 +42,6 @@ DB_NAME = os.environ.get("DATABASE_NAME", "tamilskmoviez2")
 FORCE_SUB_CHANNEL = lambda: int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
-
-#verified message
-VERIFIED_MSG = os.environ.get("VERIFIED_MSG", "<b>You Are Successfully Verified ❤️‍🩹\n\nYou Can Get Any Files Without Verifiy Untill Next 12Hrs.\n\nTamil Movies/Series - https://t.me/+lW8rqXwP8qQ0ODZl\n\nVijay Tv Serials/Shows - https://t.me/+Jk4hcVP8A-43ZjZl\n\nCWC Season 5 - https://t.me/+G3yps9kTPE04ZDFl\n\nTop Cooku Dupe Cooku - https://t.me/+jfivsh1ShrA0MTE1</b>")
-
-#verify message
-VERIFY_MSG = os.environ.get("VERIFY_MSG", "<b>Hello 👋🏻, You Need To Verify The Link To Get Files, Verification Will Be Expired After 12 Hours.\n\nif You Don't Know how To Verify. Click Below Button To See How To Verify The Link.</b>")
-
-#start message
-START_MSG = os.environ.get("START_MESSAGE", "<b>👋🏻 нєℓℓo {first}\n\nɪᴛ's ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.\n\n🄼🄰🄳🄴 🄱🅈 @Rapid_Bots</b>")
-try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "").split()):
-        ADMINS.append(int(x))
-except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
-
-#Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>👋🏻 нєℓℓo {first},\n\nYou Need To Join My Channels To Use This Bot 🤖.\n\nPlease Join This Channels 👇🏻 And Come Back Here...</b>")
-
-#set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", """<b>@TamilSk_Moviez - {previouscaption}
-
-👉 Subscribe Our YouTube Channel - https://youtube.com/@SkNetwork2021
-
-❤️ Share With Friends ❤️👉</b>‌‌""")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = lambda: True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
